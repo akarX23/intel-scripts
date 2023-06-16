f2c1ce093aa2f6860c4899290a877bab7ee3032c
akarX23 <63159924+akarX23@users.noreply.github.com>
2023-06-17 01:53:19 +0530

    Update install_opensearch_manual.sh

--- opensearch/manual_setup/install_opensearch_manual.sh
+++ opensearch/manual_setup/install_opensearch_manual.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-export http_proxy="http://proxy01.iind.intel.com:911/"
-export https_proxy="http://proxy01.iind.intel.com:912/"
-export ftp_proxy="ftp://proxy-us.intel.com:911/"
+export http_proxy=""
+export https_proxy=""
+export ftp_proxy=""
 export no_proxy="127.0.0.1,localhost"
 
 # For curl
-export HTTP_PROXY="http://proxy01.iind.intel.com:911/"
-export HTTPS_PROXY="http://proxy01.iind.intel.com:912/"
-export FTP_PROXY="ftp://proxy-us.intel.com:911/"
+export HTTP_PROXY=""
+export HTTPS_PROXY=""
+export FTP_PROXY=""
 export NO_PROXY="127.0.0.1,localhost"
 
 DEB_FILE="opensearch-2.6.0-linux-x64.deb"
 
 if [ ! -f "$DEB_FILE" ]; then
-  wget https://artifacts.elastic.co/downloads/opensearch/opensearch-2.6.0-linux-x64.deb
-  sudo dpkg -i opensearch-2.6.0-linux-x64.deb
+  wget https://artifacts.opensearch.org/releases/bundle/opensearch/2.8.0/opensearch-2.8.0-linux-x64.deb
 fi
 
-#sudo systemctl enable opensearch
-#sudo systemctl start opensearch
-
+sudo dpkg -i opensearch-2.6.0-linux-x64.deb
