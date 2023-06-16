0c58b4a69061fa0934ef37eb2c434aeea4d0c4b4
akarX23 <ritikagrawal1292000@gmail.com>
2023-06-16 20:42:13 +0000

    Updated setup_single_sever.sh

--- vm-provisioning/setup_single_server.sh
+++ vm-provisioning/setup_single_server.sh
@@ -13,24 +13,24 @@ done
 
 cat <<EOT >> /etc/profile.d/proxy_setup.sh
 
-export http_proxy="http://proxy01.iind.intel.com:911/"
-export https_proxy="http://proxy01.iind.intel.com:912/"
-export ftp_proxy="ftp://proxy-us.intel.com:911/"
+export http_proxy=""
+export https_proxy=""
+export ftp_proxy=""
 export no_proxy="127.0.0.1,localhost"
 
 # For curl
-export HTTP_PROXY="http://proxy01.iind.intel.com:911/"
-export HTTPS_PROXY="http://proxy01.iind.intel.com:912/"
-export FTP_PROXY="ftp://proxy-us.intel.com:911/"
-export NO_PROXY=\$no_proxy
+export HTTP_PROXY="$http_proxy"
+export HTTPS_PROXY="$https_proxy"
+export FTP_PROXY="$ftp_proxy"
+export NO_PROXY="$no_proxy"
 
 EOT
 
 cat <<EOT >> /etc/apt/apt.conf.d/80proxy
 
-Acquire::http::proxy "http://proxy01.iind.intel.com:911/";
-Acquire::https::proxy "http://proxy01.iind.intel.com:912/";
-Acquire::ftp::proxy "ftp://proxy-us.intel.com:911/";
+Acquire::http::proxy "$http_proxy";
+Acquire::https::proxy "$https_proxy";
+Acquire::ftp::proxy "$ftp_proxy";
 
 EOT
 
