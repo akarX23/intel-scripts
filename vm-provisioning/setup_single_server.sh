8790ad43c94401339dbef1b6e38e41b1b259f26a
akarX23 <ritikagrawal1292000@gmail.com>
2023-06-16 22:48:37 +0000

    fixed bug

--- vm-provisioning/setup_single_server.sh
+++ vm-provisioning/setup_single_server.sh
@@ -11,12 +11,17 @@ while getopts ":u:" opt; do
     esac
 done    
 
+http_proxy="http://proxy01.iind.intel.com:911/"
+https_proxy="http://proxy01.iind.intel.com:912/"
+ftp_proxy="ftp://proxy-us.intel.com:911/"
+no_proxy="127.0.0.1,localhost"
+
 cat <<EOT >> /etc/profile.d/proxy_setup.sh
 
-export http_proxy=""
-export https_proxy=""
-export ftp_proxy=""
-export no_proxy="127.0.0.1,localhost"
+export http_proxy="$http_proxy"
+export https_proxy="$https_proxy"
+export ftp_proxy="$ftp_proxy"
+export no_proxy="$no_proxy"
 
 # For curl
 export HTTP_PROXY="$http_proxy"
