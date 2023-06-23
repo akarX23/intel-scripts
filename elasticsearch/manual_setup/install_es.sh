8b0f63b9919f1b65141ef244303054977b44bfb3
akarX23 <63159924+akarX23@users.noreply.github.com>
2023-06-23 16:14:15 +0530

    added support for CentIS

--- elasticsearch/manual_setup/install_es.sh
+++ elasticsearch/manual_setup/install_es.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-export http_proxy="http://proxy01.iind.intel.com:911/"
-export https_proxy="http://proxy01.iind.intel.com:912/"
-export ftp_proxy="ftp://proxy-us.intel.com:911/"
+export http_proxy=""
+export https_proxy=""
+export ftp_proxy=""
 export no_proxy="127.0.0.1,localhost"
 
 # For curl
-export HTTP_PROXY="http://proxy01.iind.intel.com:911/"
-export HTTPS_PROXY="http://proxy01.iind.intel.com:912/"
-export FTP_PROXY="ftp://proxy-us.intel.com:911/"
+export HTTP_PROXY=""
+export HTTPS_PROXY=""
+export FTP_PROXY=""
 export NO_PROXY="127.0.0.1,localhost"
 
-DEB_FILE="elasticsearch-8.8.1-amd64.deb"
+DEB_FILE="elasticsearch-7.17.10-amd64.deb"
+RPM_FILE="elasticsearch-7.17.10-x86_64.rpm"
 
-if [ ! -f "$DEB_FILE" ]; then
-  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.8.1-amd64.deb
-  sudo dpkg -i elasticsearch-8.8.1-amd64.deb
+if [ ! -f "$DEB_FILE" ] && [ ! -f "$RPM_FILE" ]; then
+  if [[ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]]; then
+    wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.17.10-amd64.deb
+    sudo dpkg -i elasticsearch-7.17.10-amd64.deb
+  elif [[ "$(grep -Ei 'centos|redhat' /etc/*release)" ]]; then
+    wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.17.10-x86_64.rpm
+    sudo rpm --install elasticsearch-7.17.10-x86_64.rpm
+  else
+    echo "Unsupported operating system"
+    exit 1
+  fi
 fi
 
 #sudo systemctl enable opensearch
