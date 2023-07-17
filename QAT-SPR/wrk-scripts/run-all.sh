471d93757da609bf7fc057e0beb32854d3a7627b
Ritik <you@example.com>
2023-07-17 15:14:16 +0530

    fixing run-all.sh

--- QAT-SPR/wrk-scripts/run-all.sh
+++ QAT-SPR/wrk-scripts/run-all.sh
@@ -5,9 +5,9 @@ ulimit -n 655350
 # Default values
 server="localhost:443"
 duration=10
-nginx_bin_path="/home/akarx/QAT-installs/NGINX/install/sbin/nginx"
-nginx_qat_conf_path="/home/akarx/QAT-installs/NGINX/install/conf/nginx.conf.qat"
-nginx_wqat_cong_path="/home/akarx/QAT-installs/NGINX/install/conf/nginx.conf.bak"
+nginx_bin_path="/home/akarx/QAT-installs/NGINX/sbin/nginx"
+nginx_qat_conf_path="/home/akarx/QAT-installs/NGINX/conf/qat.conf"
+nginx_wqat_cong_path="/home/akarx/QAT-installs/NGINX/conf/wqat.conf"
 threads=28
 connections=2000
 
@@ -102,7 +102,7 @@ echo "Enabling QAT..."
 eval $nginx_bin_path -s stop 2> /dev/null
 cp /home/akarx/QAT-installs/Engine/qat_hw_config/4xxx/multi_process/4xxx_dev0.conf /etc/4xxx_dev1.conf
 cp /home/akarx/QAT-installs/Engine/qat_hw_config/4xxx/multi_process/4xxx_dev0.conf /etc/4xxx_dev0.conf
-sleep 5
+sleep 2
 service qat_service restart
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
@@ -112,7 +112,7 @@ echo -e "+++++++++++++++++++++++++++++++++++++++++++++\n"
 eval $nginx_bin_path -c $nginx_qat_conf_path
 
 mkdir -p logs
-sar  -n DEV $(($(( $duration )) * 4)) 1 > logs/qat_sar.log &
+# sar  -n DEV $(($(( $duration )) * 4)) 1 > logs/qat_sar.log &
 
 run_workloads --with-qat
 # cat /sys/kernel/debug/qat_4xxx_0000:6b:00.0/fw_counters
@@ -130,7 +130,7 @@ echo -e "+++++++++++++++++++++++++++++++++++++++++++++\n"
 flush_cache
 eval $nginx_bin_path -c $nginx_wqat_cong_path
 
-sar  -n DEV $(($(( $duration )) * 4)) 1 > logs/sar.log &
+# sar  -n DEV $(($(( $duration )) * 4)) 1 > logs/sar.log &
 
 run_workloads
 
