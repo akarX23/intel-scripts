c78709c1ffe16834976209b2ce7bfd8b5e78bc59
Ritik <you@example.com>
2023-07-17 16:43:24 +0530

    added generate file script

---
+++ system/gen-file-size.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+output_file="$2"
+filesize_kb=$2
+
+# Calculate the number of lines needed to fill the file with zeroes
+lines=$((filesize_kb * 1024 / 2)) # Divide by 2 because each line consists of 2 characters: '0\n'
+
+# Generate the content with zeroes and write to the file
+for (( i=1; i<=$lines; i++ ))
+do
+  echo -ne "0\n" >> "$output_file"
+done
