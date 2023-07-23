d49378a6ab08eac106d621f88cb7e746cb66cf02
akarX23 <63159924+akarX23@users.noreply.github.com>
2023-07-23 15:32:33 +0530

    breaking changes in setup scripts

--- HammerDB/install-hammerDB.sh
+++ HammerDB/install-hammerDB.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/TPC-Council/HammerDB/releases/download/v4.8/HammerDB-4.1-Linux.tar.gz
-tar -zxvf HammerDB-4.1-Linux.tar.gz 
+wget https://github.com/TPC-Council/HammerDB/releases/download/v4.8/HammerDB-4.8-Linux.tar.gz
+tar -zxvf HammerDB-4.8-Linux.tar.gz 
 cd HammerDB-4.1
-apt-get update 
-apt-get install libmysqlclient21 -y
+sudo apt-get update 
+sudo apt-get install libmysqlclient21 -y
