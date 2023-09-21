86cad0ca88d3e9b4c06d6c5bf167f8ff3c1c5499
Ritik <ritikagrawal1292000@gmail.com>
2023-09-21 22:55:37 +0530

    Added benchmark scripts for rocksdb qat

---
+++ QAT-SPR/rocksdb-qat/benchmark_fill.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Function to display the help message
+display_help() {
+    echo "Usage: ./benchmark_fill.sh [options]"
+    echo "Options:"
+    echo "  --data-dir, -d             Set the database directory (default: $DATABASE_DIR)"
+    echo "  --rocksdb-dir, -r          Set the RocksDB directory (default: $ROCKSDB_DIR)"
+    echo "  --max-ops, -m              Set the maximum number of operations (default: $MAX_OPS)"
+    echo "  --threads, -t              Set the number of threads (default: $NUM_THREADS)"
+    echo "  --max-bg-jobs, -j          Set the maximum number of background jobs (default: $MAX_BG_JOBS)"
+    echo "  --bench-type. -b           Set the benchmark type (default: $BENCH_TYPE)"
+    echo "  --numa-args, -na           Set the numa arguments (default: $NUMA_ARGS)"
+    echo "  --help, -h                 Display this help message"
+}
+
+DATABASE_DIR=/tmp
+NUMA_ARGS="--cpunodebind=0 --membind=0"
+ROCKSDB_DIR="/home/akarx/QAT-installs/RocksDB-git"
+MAX_OPS=275000000
+NUM_THREADS=1
+MAX_BG_JOBS=30
+BENCH_TYPE="qat"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        --numa-args | -na )
+            NUMA_ARGS="$2"
+            shift 1
+            ;;
+        --data-dir | -d )
+            DATABASE_DIR="$2"
+            shift 1
+            ;;
+        --rocksdb-dir | -r )
+            ROCKSDB_DIR="$2"
+            shift 1
+            ;;
+        --max-ops | -m )
+            MAX_OPS="$2"
+            shift 1
+            ;;
+        --threads | -t )
+            NUM_THREADS="$2"
+            shift 1
+            ;;
+        --max-bg-jobs | -j )
+            MAX_BG_JOBS="$2"
+            shift 1
+            ;;
+        --bench-type | -b )
+            BENCH_TYPE="$2"
+            shift 1
+            ;;
+        --help | -h )
+            display_help
+            exit 0
+            ;;
+        * )
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [ "$BENCH_TYPE" == "qat" ]; then
+  COMPRESSION_TYPE="com.intel.qat_compressor_rocksdb"
+  COMPRESSION_OPTIONS="execution_path=hw;compression_mode=dynamic;level=0"
+elif [ "$BENCH_TYPE" == "zstd" ]; then
+  COMPRESSION_TYPE="zstd"
+  COMPRESSION_OPTIONS=""
+else
+  echo "Invalid benchmark type"
+  exit 1
+fi
+
+sudo sh -c "sync;echo 3 > /proc/sys/vm/drop_caches"
+
+i=0
+# Fillseq
+echo "PREPARE DATA"
+# for (( i = 0; i < $NUM_IAA; i++ ))
+# do
+    # if [ "$i" -eq 0 ]; then
+    # NUMA_ARGS="-C 0-55"
+    # else
+    # NUMA_ARGS="-C 56-111"
+    # fi
+
+    rm -rf "$DATABASE_DIR/rocksdb_${BENCH_TYPE}_${i}"
+    mkdir -p "$DATABASE_DIR/rocksdb_${BENCH_TYPE}_${i}"
+    numactl $NUMA_ARGS "$ROCKSDB_DIR/db_bench" --benchmarks="fillseq" --db="$DATABASE_DIR/rocksdb_${BENCH_TYPE}_${i}" \
+    --key_size=16 --value_size=32 --block_size=16384 --num="$MAX_OPS" --bloom_bits=10 --threads="$NUM_THREADS" --disable_wal \
+    --value_src_data_type=file_direct --value_src_data_file=standard_calgary_corpus \
+    --compression_type="$COMPRESSION_TYPE" --compressor_options="$COMPRESSION_OPTIONS" \
+    --cache_size=-1 --cache_index_and_filter_blocks=false --compressed_cache_size=-1 --row_cache_size=0 \
+    --use_direct_reads=false --use_direct_io_for_flush_and_compaction=false \
+    --max_background_jobs="$MAX_BG_JOBS" --subcompactions=5 &
+    pids[${i}]=$!
+# done
+
+for pid in ${pids[*]}; do
+  wait $pid
+done
---
+++ QAT-SPR/rocksdb-qat/summarise.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+log_dir=logs
+VERBOSE=false
+ROCKSDB_DIR="/home/akarx/QAT-installs/RocksDB-git"
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    --log-dir)
+      log_dir="$2"
+      shift # past argument
+      shift
+      ;;
+    --rocksdb-dir | -r )
+        ROCKSDB_DIR="$2"
+        shift 
+        shift
+        ;;
+    --verbose | -v)
+      VERBOSE=true
+      shift # past argument
+      ;;
+    *) # unknown option
+      shift # past argument
+      ;;
+  esac
+done
+
+# Count IAA devices
+NUM_QAT=$(lspci | grep "rev 40" | wc -l)
+
+echo -e "\n$(hostnamectl | grep "Operating System")"
+echo "Kernel Version: $(hostnamectl | grep "Kernel" | cut -d ":" -f 2 | sed -e 's/^[[:space:]]*//')"
+echo "RocksDB Version: $($ROCKSDB_DIR/db_bench --version | cut -d " " -f 3)"
+echo "ZSTD Version: $(zstd --version | grep -oP 'v\d+\.\d+\.\d+')"
+echo "CPU: $(lscpu | grep "Model name" | cut -d ":" -f 2 | tr -s " " | head -n 1)"
+echo "Number of Enabled QAT devices: $NUM_QAT"
+
+tests=("iaa" "zstd")
+
+# Declare an associative array to store the extracted values
+declare -A extracted_values
+metrics=("tpt_rw" "dbs_size" "cpu_tot" "avg_p99_get_latency")
+metric_headers=("Read Write Throughput (ops/s)" "Compressed Data Size (GB)" "CPU utilization (%)" "p99 get latency (us)")
+
+for test in "${tests[@]}"
+do
+    tpt_rw=$(cat $log_dir/output_${test}_* | grep readrandomwriterandom | tr -s " " | cut -d " " -f 5 | paste -s -d+ - | bc)
+    cpu_usr=$(cat $log_dir/cpu_util_${test}.txt | grep Average | tr -s ' ' | cut -d ' ' -f 3)
+    cpu_sys=$(cat $log_dir/cpu_util_${test}.txt | grep Average | tr -s ' ' | cut -d ' ' -f 5)
+    cpu_tot=$(echo "$cpu_usr+$cpu_sys" | bc)
+    sum_p99_get_latency=$(cat $log_dir/output_${test}_* | grep rocksdb.db.get.micros | cut -d ' ' -f 10 | paste -s -d+ - | bc)
+    avg_p99_get_latency=$(echo "scale=2; $sum_p99_get_latency/$NUM_QAT" | bc)
+    dbs_size=$(cat $log_dir/dbs_size_${test})
+
+    if [[ "$VERBOSE" = true ]]; then
+      echo "Test: $(echo "$test" | tr '[:lower:]' '[:upper:]')"
+    fi
+  
+    for index in "${!metrics[@]}"
+    do
+        metric="${metrics[${index}]}"
+        header="${metric_headers[${index}]}"
+
+        extracted_values["${metric}_${test}"]=$(eval echo "\${${metric}}")
+
+        if [[ "$VERBOSE" = true ]]; then
+          echo "$header: ${extracted_values["${metric}_${test}"]}"
+        fi
+    done
+    echo
+done
+
+# Print table
+table_width=71
+
+echo "+$(printf "%0.s-" $(seq 1 $table_width))+"
+printf "| %30s | %10s | %10s | %10s |\n" "Metric" "IAA" "ZSTD" "% Change"
+echo "+$(printf "%0.s-" $(seq 1 $table_width))+"
+
+for index in "${!metrics[@]}"
+do
+    metric="${metrics[${index}]}"
+    header="${metric_headers[${index}]}"
+
+    iaa_val=${extracted_values["${metric}_iaa"]}
+    zstd_val=${extracted_values["${metric}_zstd"]}
+    percent_change=$(echo "scale=2; (($iaa_val-$zstd_val)/$zstd_val)*100" | bc)
+
+    if (( $(echo "$percent_change >= 0" | bc -l) )); then
+    percent_change="+$percent_change"
+    fi
+
+    printf "| %30s | %10s | %10s | %10s |\n" "$header" "$iaa_val" "$zstd_val" "$percent_change"
+done
+
+echo "+$(printf "%0.s-" $(seq 1 $table_width))+"
