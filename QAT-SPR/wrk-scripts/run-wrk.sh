65deb9c16a5f7772705183b182b6d1e64e9f6238
Ritik <ritikagrawal1292000@gmail.com>
2023-10-12 15:32:17 +0530

    fix bug

--- QAT-SPR/wrk-scripts/run-wrk.sh
+++ QAT-SPR/wrk-scripts/run-wrk.sh
@@ -111,16 +111,12 @@ function countdown {
     printf "\n"
 }
 
-LOG_DIR="$log_pre-$(date +%Y-%m-%d-%H:%M:%S)"
-
-mkdir $LOG_DIR
+mkdir $log_pre
 
 # Run wrk and save output to log file
 
-echo -e "\nExecuting WRK test"
-log_file="${size}${with_qat}_query.log"
-wrk -t $threads -c $connections -d ${duration}s  -L --timeout 4s \
- -H "Connection: keep-alive" "https://$server/$size" > "$LOG_DIR/$log_file" 2>&1 &
+echo -e "\nExecuting WRK test" \
+ -H "Connection: keep-alive" "https://$server/$size" > "$log_pre/$log_file" 2>&1 &
 pid=$!
 countdown $duration $pid
 
