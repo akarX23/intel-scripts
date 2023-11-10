2876723f32977daed5761d44044e024ab07d95ec
akarX23 <63159924+akarX23@users.noreply.github.com>
2023-11-10 23:42:42 +0530

    Update setup-ssb.sh

--- Clickhouse/setup-ssb.sh
+++ Clickhouse/setup-ssb.sh
@@ -2,7 +2,8 @@
 
 SSB_DIR=$(pwd)/ssb-dbgen
 SIZE_FACTOR=20
-ONLY_GEN_DATA=0
+GEN_DATA=0
+BUILD_SSB=0
 FILL_SERVER=false
 CLICKHOUSE_BINARY="$(which clickhouse)"
 ckhost="localhost"
@@ -18,7 +19,8 @@ function display_help {
     echo "Options:"
     echo "  -s, --ssb-dir PATH       Specify the SSB dbgen directory (default: $SSB_DIR)"
     echo "  -f, --size-factor VALUE  Specify the size factor (default: $SIZE_FACTOR)"
-    echo "  -o, --only-gen-data      Pass this to only generate data without building the library"
+    echo "  -o, --gen-data           Pass this to only generate data"
+    echo "  -b                       Pass this to build the dbgen binary"
     echo "  --fill-server            Fill a clickhouse server with ssb data"
     echo "  --clickhouse-bin         Set the path to the ClickHouse binary. Default: $CLICKHOUSE_BINARY"
     echo "  --ck-host                Clickhouse host (default: $ckhost)"
@@ -39,8 +41,11 @@ while [[ $# -gt 0 ]]; do
             SIZE_FACTOR="$2"
             shift
             ;;
-        -o|--only-gen-data)
-            ONLY_GEN_DATA=1
+        -o|--gen-data)
+            GEN_DATA=1
+            ;;
+        -b)
+            BUILD_SSB=1
             ;;
         -h|--help)
             display_help
@@ -70,39 +75,6 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-# Display configuration
-echo "Configuring with the following settings:"
-echo "SSB_DIR: $SSB_DIR"
-echo "SIZE_FACTOR: $SIZE_FACTOR"
-echo "ONLY_GEN_DATA: $ONLY_GEN_DATA"
-
-# Clone and build when ONLY_GEN_DATA is not set
-if [ $ONLY_GEN_DATA -eq 0 ]; then
-    pprint "Cloning ssb at: $SSB_DIR"
-    git clone https://github.com/vadimtk/ssb-dbgen.git $SSB_DIR
-
-    cd $SSB_DIR
-    pprint "Building ssb with command: make"
-    make
-fi
-
-cd $SSB_DIR
-DB_GEN_COMMAND="${SSB_DIR}/dbgen -s $SIZE_FACTOR"
-pprint "Generating data with command: $DB_GEN_COMMAND"
-pprint "Generating customer data: $SSB_DIR/customer.tbl"
-eval $DB_GEN_COMMAND -T c
-pprint "Generating lineorder data: $SSB_DIR/lineorder.tbl"
-eval $DB_GEN_COMMAND -T l
-pprint "Generating part data: $SSB_DIR/part.tbl"
-eval $DB_GEN_COMMAND -T p
-pprint "Generating supplier data: $SSB_DIR/supplier.tbl"
-eval $DB_GEN_COMMAND -T s
-pprint "Generating date data: $SSB_DIR/date.tbl"
-eval $DB_GEN_COMMAND -T d
-
-if [[ $FILL_SERVER == false ]]; then
-    exit 1;
-fi
 
 supplier_table="
    CREATE TABLE supplier
@@ -228,12 +200,44 @@ lineorder_flat_table="
     show settings ilike 'max_memory_usage';
 "
 
-# Insert data
+# Display configuration
+echo "Configuring with the following settings:"
+echo "SSB_DIR: $SSB_DIR"
+echo "SIZE_FACTOR: $SIZE_FACTOR"
+echo "ONLY_GEN_DATA: $ONLY_GEN_DATA"
 
-eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --multiquery --query $create_table_query
-eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --query "INSERT INTO customer FORMAT CSV" < $SSB_DIR/customer.tbl
-eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --query "INSERT INTO part FORMAT CSV" < $SSB_DIR/part.tbl
-eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --query "INSERT INTO supplier FORMAT CSV" < $SSB_DIR/supplier.tbl
-eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --query "INSERT INTO lineorder FORMAT CSV" < $SSB_DIR/customer.tbl
-eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --multiquery --query $lineorder_flat_table
+if [ $BUILD_SSB -eq 1 ]; then
+    pprint "Cloning ssb at: $SSB_DIR"
+    git clone https://github.com/vadimtk/ssb-dbgen.git $SSB_DIR
+
+    cd $SSB_DIR
+    pprint "Building ssb with command: make"
+    make
+fi
+
+# Clone and build when ONLY_GEN_DATA is not set
+if [ $GEN_DATA -eq 1 ]; then
+    cd $SSB_DIR
+    DB_GEN_COMMAND="${SSB_DIR}/dbgen -s $SIZE_FACTOR"
+    pprint "Generating data with command: $DB_GEN_COMMAND"
+    pprint "Generating customer data: $SSB_DIR/customer.tbl"
+    eval $DB_GEN_COMMAND -T c
+    pprint "Generating lineorder data: $SSB_DIR/lineorder.tbl"
+    eval $DB_GEN_COMMAND -T l
+    pprint "Generating part data: $SSB_DIR/part.tbl"
+    eval $DB_GEN_COMMAND -T p
+    pprint "Generating supplier data: $SSB_DIR/supplier.tbl"
+    eval $DB_GEN_COMMAND -T s
+    pprint "Generating date data: $SSB_DIR/date.tbl"
+    eval $DB_GEN_COMMAND -T d
+fi
+
+if [[ $FILL_SERVER == true ]]; then
+    eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --multiquery --query $create_table_query
+    eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --query "INSERT INTO customer FORMAT CSV" < $SSB_DIR/customer.tbl
+    eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --query "INSERT INTO part FORMAT CSV" < $SSB_DIR/part.tbl
+    eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --query "INSERT INTO supplier FORMAT CSV" < $SSB_DIR/supplier.tbl
+    eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --query "INSERT INTO lineorder FORMAT CSV" < $SSB_DIR/customer.tbl
+    eval $CLICKHOUSE_BINARY client --host $ckhost --port $ckport --multiquery --query $lineorder_flat_table
+fi
 
